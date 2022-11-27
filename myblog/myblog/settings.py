0132fc3c2a4cf43c5af50fb9fc4b4a002e1799c2
Adnene_wsl <adnen.guessoum@gmail.com>
2022-11-27 20:23:14 +0100

    Set up Postgresql and Docker containers
    very painful

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':  os.environ.get('SQL_ENGINE'),
+        'NAME': os.environ.get('SQL_DATABASE'),
+		'USER': os.environ.get('SQL_USER'),
+		'PASSWORD': os.environ.get('SQL_PASSWORD'),
+		'HOST': os.environ.get('SQL_HOST'),
+		'PORT': os.environ.get('SQL_PORT'),
     }
 }
 
