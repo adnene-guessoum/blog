04b98d3e3e0d07b09a311327b3a2a12fd19fa0f6
Adnene_wsl <adnen.guessoum@gmail.com>
2022-12-03 18:08:55 +0100

    some views

--- myblog/backend/views.py
+++ myblog/backend/views.py
@@ -6,6 +6,10 @@ from django.contrib.auth import get_user_model
 from rest_framework import viewsets
 from .serializers import UserSerializer
 
+from rest_framework.response import Response
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 User = get_user_model()
 
