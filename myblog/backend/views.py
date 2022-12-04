3b3c52e4150764eea03cb2219367927937bba18d
Adnene_wsl <adnen.guessoum@gmail.com>
2022-12-04 20:18:28 +0100

    Add views for blogposts

--- myblog/backend/views.py
+++ myblog/backend/views.py
@@ -10,11 +10,11 @@ from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+from backend.models import Blogpost
+from backend.serializers import BlogPostSerializer
 
 User = get_user_model()
 
-
-# Create your views here.
 
 # Disabling 7 ancestor limit for view inheritance
 class UserViewSet(viewsets.ModelViewSet):  # pylint: disable=too-many-ancestors
@@ -24,3 +24,48 @@ class UserViewSet(viewsets.ModelViewSet):  # pylint: disable=too-many-ancestors
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class BlogPostListView(ListAPIView):
+    """Listing view of the different publications by date"""
+
+    queryset = Blogpost.objects.order_by("-date_created")
+    serializer_class = BlogPostSerializer
+    lookup_field = "slug"
+    permission_classes = (permissions.AllowAny,)
+
+
+class BlogPostDetailView(RetrieveAPIView):
+    """Detail View of the different publications by date"""
+
+    queryset = Blogpost.objects.order_by("-date_created")
+    serializer_class = BlogPostSerializer
+    lookup_field = "slug"
+    permission_classes = (permissions.AllowAny,)
+
+
+class BlogPostFeaturedView(RetrieveAPIView):
+    """View of the featured publication"""
+
+    queryset = Blogpost.objects.all().filter(featured=True)
+    serializer_class = BlogPostSerializer
+    lookup_field = "slug"
+    permission_classes = (permissions.AllowAny,)
+
+
+class BlogPostCategoryView(APIView):
+    """Categories view"""
+
+    serializer_class = BlogPostSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        """post response on request"""
+        data = self.request.data
+        category = data("category")
+        queryset = Blogpost.objects.order_by("-date_created").filter(
+            category__iexact=category
+        )
+        serializer = BlogPostSerializer(queryset, any=True)
+
+        return Response(serializer.data)
