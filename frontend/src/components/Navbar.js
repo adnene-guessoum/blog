f503e464fb470ee58562e17c268e83f3689588bd
Adnene_wsl <adnen.guessoum@gmail.com>
2022-12-11 14:45:10 +0100

    Add Bootstrap Navbar for blog frontend

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,8 +2,21 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
 const navbar = () => (
-	<nav>
-		Navbar
+	<nav className="navbar navbar-expand-lg navbar-light bg-light">
+	<Link className="navbar-brand" to='/'>Owling Blog</Link>
+	<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	<span className="navbar-toggler-icon"></span>
+	</button>
+	<div className="collapse navbar-collapse" id="navbarNav">
+	<ul className="navbar-nav">
+	<li className="nav-item active">
+	<NavLink className="nav-link" exact to='/'>Home</NavLink>
+	</li>
+	<li className="nav-item">
+	<NavLink className="nav-link" exact to='/blog'>Blog</NavLink>
+	</li>
+	</ul>
+	</div>
 	</nav>
 );
 
