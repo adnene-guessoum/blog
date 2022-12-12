7394e7eaaf0f1e5c01d6fabd3afe2bd2ea9d923c
Adnene_wsl <adnen.guessoum@gmail.com>
2022-12-12 19:03:56 +0100

    Add detail frontend page for blog posts

--- frontend/src/components/BlogDetail.js
+++ frontend/src/components/BlogDetail.js
@@ -1,9 +1,45 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import axios from 'axios';
 
-const blogDetail = () => (
-	<div>Blog detail</div>
-);
+const BlogDetail = (props) => {
+    const [blog, setBlog] = useState({});
+		const { id } = useParams()
 
-export default blogDetail;
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/${id}`);
+                setBlog(res.data);
+            }
+            catch (err) {
 
+            }
+        };
+
+        fetchData();
+    }, [{id}]);
+
+    const createBlog = () => {
+        return {__html: blog.content}
+    };
+
+    const capitalizeFirstLetter = (word) => {
+        if (word)
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        return '';
+    };
+
+    return (
+        <div className='container mt-3'>
+            <h1 className='display-2'>{blog.title}</h1>
+            <h2 className='text-muted mt-3'>Catégorie: {capitalizeFirstLetter(blog.category)}</h2>
+            <h4>Le {blog.day} {blog.month} {blog.year}</h4>
+            <div className='mt-5 mb-5' dangerouslySetInnerHTML={createBlog()} />
+            <hr />
+            <p className='lead mb-5'><Link to='/blog' className='font-weight-bold'>Retour aux articles</Link></p>
+        </div>
+    );
+};
+
+export default BlogDetail;
