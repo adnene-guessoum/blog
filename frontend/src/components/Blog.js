d4fa82afd067cdb07470189629239459f926ae1d
Adnene_wsl <adnen.guessoum@gmail.com>
2022-12-12 17:08:48 +0100

    Add publications list display on blog homepage

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -26,8 +26,8 @@ const Blog = () => {
 	useEffect(() => {
 			const fetchBlogs = async () => {
 				try {
-					const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/`);
-					setBlogs(response.data);
+					const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/`);
+					setBlogs(res.data);
 				}
 				catch (err) {
 
@@ -46,7 +46,41 @@ const Blog = () => {
 	};
 
 	const getBlogs = () => {
-		
+		let list = [];
+		let result = [];
+
+		blogs.map(blogPost => {
+			return list.push(
+				<div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+				<div className="col p-4 d-flex flex-column position-static">
+          <strong className="d-inline-block mb-2 text-success">
+				{capitalizeFirstLetter(blogPost.category)}
+				</strong>
+          <h3 className="mb-0">{blogPost.title}</h3>
+          <div className="mb-1 text-muted">{blogPost.month} {blogPost.day}</div>
+          <p className="mb-auto">{blogPost.excerpt}</p>
+          <Link to={`/blog/${blogPost.slug}`} className="stretched-link">Lire la suite...</Link>
+				</div>
+				<div class="col-auto d-none d-lg-block">
+				<img width='200' height='250' src={blogPost.thumbnail} alt='thumbnail publication' />
+				</div>
+				</div>
+			);
+		});
+
+		for (let i = 0; i < list.length; i += 2) {
+            result.push(
+                <div key={i} className='row mb-2'>
+                    <div className='col-md-6'>
+                        {list[i]}
+                    </div>
+                    <div className='col-md-6'>
+                        {list[i+1] ? list[i+1] : null}
+                    </div>
+                </div>
+            )
+        }
+		return result;
 	};
 
 	return (
@@ -70,6 +104,7 @@ const Blog = () => {
 		</div>
 		</div>
 
+		{getBlogs()}
 		</div>
 	);
 
