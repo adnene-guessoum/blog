22c2a9aae98d36885d6255caf6b4cec928e92855
Adnene_wsl <adnen.guessoum@gmail.com>
2022-12-31 13:47:03 +0100

    Fix contenerization for DRF, nginx, react, psql

--- api/myblog/myblog/settings.py
+++ api/myblog/myblog/settings.py
@@ -155,11 +155,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "myblog/static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "/static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "myblog/media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "/media")
 
 # Summernotes
 SUMMERNOTE_THEME = "bs4"
