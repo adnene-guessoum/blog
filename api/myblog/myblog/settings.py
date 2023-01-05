74b4a035547de1c4acb7b21f76957d7d758d5cae
Adnene_wsl <adnen.guessoum@gmail.com>
2023-01-05 14:43:37 +0100

    hook up AWS RDS database

--- api/myblog/myblog/settings.py
+++ api/myblog/myblog/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
-ENVIRONMENT = "dev"
+ENVIRONMENT = "prod"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,11 +142,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "/static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "/media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Summernotes
 SUMMERNOTE_THEME = "bs4"
